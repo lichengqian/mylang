3784015e0aa624469cc5fa7e4694dfdc156130c6
liming <lichengqian2008@icloud.com>
2017-06-06 10:45:12 +0800

    remove internal.clj

--- net/transport/tcp/types.clj
+++ net/transport/tcp/types.clj
@@ -87,6 +87,28 @@
     EventTransportFailed
     (EventConnectionLost EndPointAddress))
 
+;;; internal
+(enum ControlHeader
+    CreateNewConnection
+    CloseConnection
+    CloseSocket
+    CloseEndPoint
+    ProbeSocket
+    ProbeSocketAct)
+
+(encode! ControlHeader)
+(decode! ControlHeader)
+    
+(enum ConnectionRequestResponse
+    "Response sent by /B/ to /A/ when /A/ tries to connect"
+    ConnectionRequestAccepted
+    ConnectionRequestInvalid
+    ConnectionRequestCrossed
+    ConnectionRequestHostMismatch)
+
+(encode! ConnectionRequestResponse)
+(decode! ConnectionRequestResponse)
+
 ; (native :golang " 
 ; type test string
 ;     ")
