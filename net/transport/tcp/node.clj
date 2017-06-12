9222bc6f3e206569108e6cd19346694c13ab90de
liming <lichengqian2008@icloud.com>
2017-06-12 20:54:51 +0800

    + node.clj

---
+++ net/transport/tcp/node.clj
@@ -0,0 +1,124 @@
+
+(type SwitchID UInt64)
+
+(defrecord LocalNode
+    [^*EndPoint localEndPoint 
+     ^LocalNodeState localState])
+
+(enum LocalNodeState
+    (LocalNodeValid ValidLocalNodeState)
+    LocalNodeClosed)
+
+(defrecord ValidLocalNodeState
+    [^"map[SwitchID]*LocalSwitch" localSwitches   
+     ^"map[SwitchID]*Connection" localConnections])
+
+(defrecord LocalSwitch
+    [^SwitchID switchID
+     ^*LocalNode switchNode
+     ^"chan Message" switchQueue])
+    ; switchState (MVar LocalSwitchState))
+
+; (struct LocalSwitchState
+;     switchQueue  (Chan Message))
+
+;;;------------------------------------------------------------------------------
+;;; Messages                                                                   --
+;;;------------------------------------------------------------------------------
+(defrecord Message
+    [^EndPointAddress msgFrom
+     ^ByteString msgPayload])
+
+(defn newLocalNode ^*LocalNode [^*Transport transport]
+    (<- endpoint (transport.NewEndPoint 0))
+    (let localNode (createBareLocalNode endpoint))
+    (go (handleNodeMessages localNode))
+    (return localNode))
+
+(defn createBareLocalNode ^*LocalNode [^*EndPoint endpoint]
+    (let 
+        st (LocalNodeValid. (map->ValidLocalNodeState 
+                                {localSwitches (native "make(map[SwitchID]*LocalSwitch)")
+                                 localConnections (native "make(map[SwitchID]*Connection)")}))
+        node (map->LocalNode
+                {localEndPoint endpoint
+                 localState &st}))
+    (return &node))
+
+;;;------------------------------------------------------------------------------
+;;; Handle incoming messages                                                   --
+;;;------------------------------------------------------------------------------
+
+(defrecord IncomingConnection
+    [^EndPointAddress theirAddress
+     ^IncomingTarget  theirTarget])
+
+(enum IncomingTarget
+    Uninit
+    (ToSwitch SwitchID))
+
+(defrecord ConnectionState
+    [^"map[ConnectionId]*IncomingConnection" incoming
+     ^"map[EndPointAddress]map[ConnectionId]struct{}" incomingFrom])
+
+(defn initConnectionState ^*ConnectionState []
+    (native
+        "return &ConnectionState {"
+        "   incoming: make(map[ConnectionId]*IncomingConnection),"
+        "   incomingFrom: make(map[EndPointAddress]map[ConnectionId]struct{}),"
+        "}"))
+
+(defn handleNodeMessages [^*LocalNode localNode]
+    (let st (initConnectionState)
+         invalidRequest (fn [^ConnectionId cid ^String msg]
+                            (delete st.incoming, cid))
+
+         deleteConn (fn [^ConnectionId cid]
+                       (delete st.incoming cid)
+                       (native 
+                           "// TODO"
+                           "// hello")))
+
+    (loop []
+        (println "handling node message...")
+        (let event (localNode.localEndPoint.Receive))
+        (match event
+            [ConnectionOpened cid ep]
+            (do
+                (assoc st.incoming cid (&IncomingConnection. ep (Uninit.)))
+                (native
+                    "if data, ok := st.incomingFrom[ep]; ok {"
+                        "data[cid] = struct{}{}"
+                    "} else {"
+                        "data = make(map[ConnectionId]struct{})"
+                        "st.incomingFrom[ep] = data"
+                        "data[cid] = struct{}{}"
+                    "}"))
+                ; (recur))
+
+            [Received cid payload]
+            (do
+                (let pConn (get st.incoming cid))
+                (if (nil? pConn)
+                    (invalidRequest cid "message received from an unknown connection")
+
+                    (match pConn.theirTarget
+                        Uninit
+                        (println "uinit")
+
+                        [ToSwitch sid]
+                        (println sid payload))))
+
+            [ConnectionClosed cid]
+            (do
+                (let pConn (get st.incoming cid))
+                (if (nil? pConn)
+                    (invalidRequest cid "closed unknown connection")
+                    (deleteConn cid)))
+
+            [ErrorEvent err msg]
+            (println err msg)
+                
+            EndPointClosed
+            return)))
+
