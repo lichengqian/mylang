e4a8d5d010a4483412f26601884a45a9632338a8
liming <lichengqian2008@icloud.com>
2017-06-19 15:59:57 +0800

    delete kotlin code

--- project.clj
+++ project.clj
@@ -3,12 +3,8 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :plugins          [[kotlinc-lein "0.1.2"]]
   :source-paths        ["src/clojure"]
-  :kotlin-source-paths ["src/kotlin"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.jetbrains.kotlin/kotlin-runtime "1.1.2"]
-                 [org.jetbrains.kotlin/kotlin-stdlib "1.1.2"]
                  [com.stuartsierra/component "0.3.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.palletops/pallet-common "0.4.0"]]
